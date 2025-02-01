82095758366dd821c0d8cda31351cf77edc74242
Shreenidhi S <165373137+sreenidhis29@users.noreply.github.com>
2025-02-01 23:46:35 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './components/Login';
-import Dashboard from './components/Dashboard';
-import Navbar from './components/Navbar';
-import './App.css';
+import React,{useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = () => {
-    return (
-        <Router>
-            <Navbar />
-            <Switch>
-                <Route path="/login" component={Login} />
-                <Route path="/dashboard" component={Dashboard} />
-            </Switch>
-        </Router>
-    );
-};
+import MonitorHome from './Components/Monitoring/MonitorHome';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Auth from './Components/Auth/Auth';
+import NoDataError from './Components/Error/NoDataError';
+import LoadingOverlay from 'react-loading-overlay';
 
-export default App;
+import './App.css'
+import { useParams } from 'react-router-dom';
+
+function App() {
+  const [isLoggedIn,setLoggedIn] = useState(false)
+  const handleLog = () => setLoggedIn(!isLoggedIn);
+  const [loading, setLoading] = useState(false);
+  const [ptId,setPtId] = useState(false)
+  const params = useParams()
+  
+  return (
+    <div className='App'>
+      <Dashboard alert = {params.alert} showall={params.show} handleLog={handleLog}/>
+      {/*!isLoggedIn ?
+       <Auth setLoggedIn={setLoggedIn}/>
+       : 
+       <>
+       {ptId ?
+        <>
+        {
+          ptId==="xxxxx"?
+          <NoDataError setPtId={setPtId}/>:
+          <LoadingOverlay
+            className='loadingOverlay'
+            active={loading}
+            spinner
+            text='Writing the datas into excel file...'
+            >
+          <MonitorHome ptId={ptId} setPtId={setPtId} loading={loading} setLoading={setLoading}/>
+          </LoadingOverlay>
+        }
+        
+        </>
+        :
+        <Dashboard handleLog={handleLog} setPtId={setPtId}/>}
+       </>
+      */} 
+      {/* <Dashboard/>  */}
+       {/* <MonitorHome ptId={ptId} setPtId={setPtId} loading={loading} setLoading={setLoading}/>  */}
+      {/* <NoDataError/>  */}
+    </div>
+    
+  )
+}
+
+export default App
